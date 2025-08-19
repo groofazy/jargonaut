e531f05bd833bad21820e3c57b68aaf09eef66b7
gabe-laptop <gabe.gamboa05@gmail.com>
2025-08-19 00:22:45 -0700

    it is 12:22 am, goodnight

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask
 
 
-def create_app(test_config=None):
+def create_app(test_config=None): # factory function
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
@@ -28,5 +28,8 @@ def create_app(test_config=None):
     @app.route('/')
     def hello():
         return 'Hello, World!'
+    
+    from . import db
+    db.init_app(app)
 
     return app

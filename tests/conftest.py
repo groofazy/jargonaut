11a53048d8ab8b90848f7abb1fd92cfe3f4c8ded
gabe-laptop <gabe.gamboa05@gmail.com>
2025-08-26 11:24:50 -0700

    updating test files

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,10 +10,10 @@ with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
 
 @pytest.fixture
 def app():
-    db_fd, db_path = tempfile.mkstemp()
+    db_fd, db_path = tempfile.mkstemp() # creates and opens a temporary file, returning file descriptor and path to it
 
     app = create_app({
-        'TESTING': True,
+        'TESTING': True, # tells Flask app is in test mode
         'DATABASE': db_path,
     })
 
@@ -28,8 +28,27 @@ def app():
 
 @pytest.fixture
 def client(app):
-    return app.test_client
+    return app.test_client # creates test client objects, allows HTTP rrequest simulation
 
 @pytest.fixture
 def runner(app):
-    return app.test_cli_runner()
+    return app.test_cli_runner() # can call Click commands registered with application
+
+class AuthActions(object): # helper class for User Login Authorization
+    def __init__(self, client):
+        self._client = client
+    
+    def login(self, username='test', password='test'): # logs in as test user POST
+        return self._client.post(
+            '/auth/login',
+            data={'username': username, 'password': password}
+        )
+    
+    def logout(self): # logs out as test user GET
+        return self._client.get('/auth/logout')
+
+
+
+@pytest.fixture
+def auth(client):
+    return AuthActions(client)
---
+++ tests/test_auth.py
@@ -0,0 +1,46 @@
+import pytest
+from flask import session
+from flaskr.db import get_db
+
+
+def test_register(client, app):
+    assert client.get('/auth/register').status_code == 200 # makes GET request and returns Response object, checks for 200 OK status code for successful page rendering 
+    response = client.post( # makes POST request, converting data dict into form data
+        '/auth/register', data={'username': 'a', 'password': 'a'}
+    )
+    assert response.headers["Location"] == "/auth/login" # checks for proper login URL after redirection
+
+    with app.app_context():
+        assert get_db().execute(
+            "SELECT * FROM user WHERE username = 'a'",
+        ).fetchone() is not None
+
+@pytest.mark.parametrize(('username', 'password', 'message'), ( # runs test function with different arguments
+    ('', '', b'Username is required.'),
+    ('a', '', b'Password is required.'),
+    ('test', 'test', b'already registered'),
+))
+def test_register_validate_input(client, username, password, message):
+    response = client.post(
+        '/auth/register',
+        data={'username': username, 'password': password}
+    )
+    assert message in response.data
+
+def test_login(client, auth):
+    assert client.get('auth/login').status_code == 200
+    response = auth.login()
+    assert response.headers["Location"] == "/"
+
+    with client:
+        client.get('/')
+        assert session['user_id'] == 1
+        assert g.user['username'] == 'test'
+
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+    ('a', 'test', b'Incorrect username.'),
+    ('test', 'a', b'Incorrect password.'),
+))
+def test_login_validate_input(auth, username, password, message):
+    response = auth.login(username, password)
+    assert message in response.data
